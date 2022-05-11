652ba1fe11aa5757bf7d17731c779bca6aee03a6
mjd166 <mghafari41@yahoo.com>
2022-05-11 08:49:27 +0330

    refactor product model

--- LampShade/ShopManagement.Infrastructure.EFCore/Migrations/20220511050449_removecolumnsunitpriceandisinstockfromproducttable.Designer.cs
+++ LampShade/ShopManagement.Infrastructure.EFCore/Migrations/20220511050449_removecolumnsunitpriceandisinstockfromproducttable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopManagement.Infrastructure.EFCore;
 
 namespace ShopManagement.Infrastructure.EFCore.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20220511050449_removecolumnsunitpriceandisinstockfromproducttable")]
+    partial class removecolumnsunitpriceandisinstockfromproducttable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
