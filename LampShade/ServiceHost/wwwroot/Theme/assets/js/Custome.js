4524bffd0df7d1d74e71de616bd31d5579d98639
MJD166 <mghafari41@yahoo.com>
2022-06-26 18:02:12 +0330

    add to basket completed with js

---
+++ LampShade/ServiceHost/wwwroot/Theme/assets/js/Custome.js
@@ -0,0 +1,95 @@
+﻿function addToCard(id, name, price, picture) {
+    debugger;
+    const cookieName = "cart_items";
+
+    let products = $.cookie(cookieName);
+
+    if (products === undefined) {
+        products = [];
+    }
+    else {
+        products = JSON.parse(products);
+
+    }
+
+    const count = $("#ProductCount").val();
+
+    const CurrentCountProduct = products.find(x => x.id === id);
+    debugger;
+    if (CurrentCountProduct !== undefined) {
+        products.find(x => x.id === id).count = parseInt(CurrentCountProduct.count) + parseInt(count);
+    } else {
+        const product = {
+            id,
+            name,
+            price,
+            picture,
+            count
+        }
+
+        products.push(product);
+
+
+    }
+
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+    UpdateCart();
+}
+
+function UpdateCart() {
+    debugger;
+    const cookieName = "cart_items";
+    let products = $.cookie(cookieName);
+
+    if (products !== undefined) {
+        products = JSON.parse(products);
+        $("#card_items_count").text(products.length);
+        let cartItemWrapper = $("#cart_items_wrapper")
+        if (cartItemWrapper !== undefined) {
+            cartItemWrapper.html('');
+            products.forEach(x => {
+                const product = `<div class="single-cart-item">
+                                <a href="javascript:void(0)" class="remove-icon" onclick="removeFromCard('${x.id}')">
+                                    <i class="ion-android-close"></i>
+                                </a>
+                                <div class="image">
+                                    <a href="single-product.html">
+                                        <img src="/ProductPictures/${x.picture}"
+                                             class="img-fluid" alt="">
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <p class="product-title">
+                                        <a href="single-product.html">محصول: ${x.name}</a>
+                                    </p>
+                                    <p class="count">تعداد: ${x.count}</p>
+                                    <p class="count">قیمت واحد: ${x.price}</p>
+                                </div>
+                            </div>`;
+
+                cartItemWrapper.append(product);
+                
+            });
+        }
+      
+    }
+}
+
+
+function removeFromCard(id) {
+    const cookieName = "cart_items";
+    let products = $.cookie(cookieName);
+    products = JSON.parse(products);
+    if (products !== undefined) {
+        debugger;
+        
+        let itemToRemove = products.findIndex(x => x.id === id);
+        
+        if (itemToRemove !== undefined) {
+            products.splice(itemToRemove, 1);
+            $.cookie(cookieName, JSON.stringify(products), { expires: 2,path: "/"});
+            UpdateCart();
+        }
+    }
+}
+
